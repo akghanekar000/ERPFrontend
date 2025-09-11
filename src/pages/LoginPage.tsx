57b51ac07cb018f482ee72c56b13743fe851dc2c
akghanekar000 <akghanekar000@gmail.com>
2025-09-11 22:15:05 +0530

    new

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,8 +5,8 @@ import { SignIn } from "@clerk/clerk-react";
 export default function LoginPage() {
   return (
     <div style={{ maxWidth: 480, margin: "2rem auto", padding: 16 }}>
-      <h1>Login</h1>
-      <SignIn path="/login" routing="path" signUpUrl="/register" />
+      <h1>Sign in</h1>
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
     </div>
   );
 }
